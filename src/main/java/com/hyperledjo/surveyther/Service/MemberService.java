a689303edd4c8597e3fcae6c25926b53bbd37813
KimH4nKyul <alskim93@kakao.com>
2021-09-29 03:32:41 +0900

    New Kakao

--- src/main/java/com/hyperledjo/surveyther/Service/MemberService.java
+++ src/main/java/com/hyperledjo/surveyther/Service/MemberService.java
@@ -2,10 +2,30 @@ package com.hyperledjo.surveyther.Service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.hyperledjo.surveyther.DAO.MemberDAO;
 import com.hyperledjo.surveyther.DTO.Member;
 
-public interface MemberService {
+@Service
+public class MemberServiceImpl implements MemberService {
 
-	public void postMember(Member member);
-	public List<Member> getMemberList();
+	private MemberDAO memberDAO;
+
+	public MemberServiceImpl(MemberDAO memberDAO) {
+		this.memberDAO = memberDAO;
+	}
+
+	@Override
+	public List<Member> getMemberList() {
+		// TODO Auto-generated method stub
+		return memberDAO.getMemberList();
+	}
+
+	@Override
+	public void postMember(Member member) {
+		// TODO Auto-generated method stub
+		memberDAO.postMember(member);
+	}
+
 }
