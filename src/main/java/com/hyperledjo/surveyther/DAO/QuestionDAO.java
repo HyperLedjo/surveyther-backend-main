0b00bc93e53cc56d0ef1005f2b4bcabb255625c5
KimH4nKyul <alskim93@kakao.com>
2021-10-04 12:11:28 +0900

    21-10-04 QuestionDAO upload

--- src/main/java/com/hyperledjo/surveyther/DAO/QuestionDAO.java
+++ src/main/java/com/hyperledjo/surveyther/DAO/QuestionDAO.java
@@ -1,8 +1,35 @@
 package com.hyperledjo.surveyther.DAO;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.hyperledjo.surveyther.DTO.Question;
 
 @Repository
 public class QuestionDAO {
 
+	private String nameSpace = "com.hyperledjo.surveyther.DAO.QuestionDAO";
+	private SqlSessionTemplate sql;
+	
+	public QuestionDAO(SqlSessionTemplate sql) {
+		this.sql = sql;
+	}
+	
+	public int postQuestion(Question question) {
+		int result = sql.insert(nameSpace + ".postQuestion", question);
+		if(0 < result) { 
+			return question.getNo();
+		}
+		return result;
+	}
+	
+	public List<Question> getQuestionFromSurvey(int id) {
+		return sql.selectList(nameSpace + ".getQuestionsFromSurvey", id);
+	}
+	
+	public List<Question> getQuestionList() {
+		return sql.selectList(nameSpace + ".getQuestionList");
+	}
 }
