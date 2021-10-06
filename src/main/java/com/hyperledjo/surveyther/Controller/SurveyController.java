435fe16fa01caa48916416234ce47d94c9a190ad
KimH4nKyul <alskim93@kakao.com>
2021-10-06 12:33:33 +0900

    21-10-06 12:32 Service 컨트롤러 구현

--- src/main/java/com/hyperledjo/surveyther/Controller/SurveyController.java
+++ src/main/java/com/hyperledjo/surveyther/Controller/SurveyController.java
@@ -3,7 +3,9 @@ package com.hyperledjo.surveyther.Controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +22,38 @@ public class SurveyController {
 		this.surveyService = surveyService;
 	}
 	
+	@PostMapping("/survey")
+	public int postSurvey(Survey survey) {
+		return surveyService.postSurvey(survey);
+	}
+	
+	@PatchMapping("/survey/{id}")
+	public Survey closeSurvey(@PathVariable("id") int id) {
+		return surveyService.closeSurvey(id);
+	}
+	
+	@GetMapping("/survey/closed")
+	public List<Survey> getClosedSurveyList() {
+		return surveyService.getClosedSurveyList();
+	}
+	
+	@GetMapping("/survey/ongoing")
+	public List<Survey> getOnGoinSurveyList() {
+		return surveyService.getOnGoingSurveyList();
+	}
+	
+	@GetMapping("/survey/my/{id}")
+	public List<Survey> getMySurveyList(@PathVariable("id") int id) {
+		return surveyService.getMySurveyList(id);
+	}
+	
 	@GetMapping("/survey/{id}")
 	public Survey getSurvey(@PathVariable("id") int id) {
-		return null;
+		return surveyService.getSurvey(id);
 	}
 	
 	@GetMapping("/survey")
 	public List<Survey> getSurveyList() {
-		return null;
+		return surveyService.getSurveyList();
 	}
 }
--- src/main/java/com/hyperledjo/surveyther/Service/SurveyService.java
+++ src/main/java/com/hyperledjo/surveyther/Service/SurveyService.java
@@ -16,11 +16,36 @@ public class SurveyService {
 		this.surveyDAO = surveyDAO;
 	}
 
+	public int postSurvey(Survey survey) {
+		return surveyDAO.postSurvey(survey);
+	}
+	
+	public Survey closeSurvey(int id) {
+		int result = surveyDAO.closeSurvey(id);
+		Survey survey = null;
+		if(0 < result) {
+			survey = surveyDAO.getSurvey(id);
+		}
+		return survey;
+	}
+	
+	public List<Survey> getClosedSurveyList() {
+		return surveyDAO.getClosedSurveyList();
+	}
+	
+	public List<Survey> getOnGoingSurveyList() {
+		return surveyDAO.getOnGoingSurveyList();
+	}
+	
+	public List<Survey> getMySurveyList(int id) {
+		return surveyDAO.getMySurveyList(id);
+	}
+	
 	public Survey getSurvey(int id) {
-		return null;
+		return surveyDAO.getSurvey(id);
 	}
 	
 	public List<Survey> getSurveyList() {
-		return null;
+		return surveyDAO.getSurveyList();
 	}
 }
